f0827aa7b5bf07def03ea5a2601cfe58b19f6d9c
Venkateswarlu Vennampalli <venkateswarlu4466@gmail.com>
2026-06-04 09:32:09 +0530

    Added admin registration for all models

--- backend/tenants/admin.py
+++ backend/tenants/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Tenant
 
-# Register your models here.
+@admin.register(Tenant)
+class TenantAdmin(admin.ModelAdmin):
+    list_display = (
+        "company_name",
+        "domain",
+        "industry",
+        "status",
+        "created_at"
+    )
+    search_fields = ("company_name", "domain")
+    list_filter = ("status",)
